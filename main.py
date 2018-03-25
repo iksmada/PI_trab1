a472b9d1aaaf9c559f98e78b3aeb483633fa2343
Raphael Adamski <raphaeladamski@hotmail.com>
2018-03-25 17:27:31 -0300

    remove matplotlib residual

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 from os import listdir, path, makedirs
 import cv2
-
-def rgb2gray(rgb):
-    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
-
 
 # select randonly a image from dirName
 dirName = 'imagens_objetos_coloridos'
@@ -15,12 +9,6 @@ imgName = images[np.trunc(np.random.rand() * len(images)).astype(int)]
 completeFilePath = dirName + '/' + imgName
 img = cv2.imread(completeFilePath)
 img_greyscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# define plot images grid
-fig = plt.figure()
-row = 2
-column = 2
-figCounter = 1
 
 # ensure save path exists
 fileName = path.splitext(imgName)
@@ -42,7 +30,3 @@ edges = ~cv2.Canny(np.uint8(binary), 100, 200)
 cv2.imshow('Edge Image', edges)
 cv2.imwrite(resultsDirName + '/' + fileName[0] + '_' + 'edges' + fileName[1], edges)
 cv2.waitKey(1000)
-
-# show grid of images
-plt.tight_layout()
-plt.show()
