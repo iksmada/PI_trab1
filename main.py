e67154a3bbe237d7e022178436f896c3cd8c4523
Raphael Adamski <raphaeladamski@hotmail.com>
2018-03-25 19:40:26 -0300

    plot histogram

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 from os import listdir, path, makedirs
 import cv2
+import matplotlib.pyplot as plt
 
 # select randonly a image from dirName
 dirName = 'imagens_objetos_coloridos'
@@ -32,28 +33,52 @@ cv2.imshow('Edge Image', edges)
 cv2.imwrite(resultsDirName + '/' + fileName[0] + '_' + 'edges' + fileName[1], edges)
 cv2.waitKey(1000)
 
-#numbered image and stats
+# numbered image and stats
 image, contours, hierarchy = cv2.findContours(binary, 1, 2)
 del contours[-1]
-i=0
-font                   = cv2.FONT_HERSHEY_SIMPLEX
-fontScale              = 0.5
-fontColor              = (255,255,255)
-lineType               = 1
+smallObj = []
+midObj = []
+bigObj = []
+i = 0
+font = cv2.FONT_HERSHEY_SIMPLEX
+fontScale = 0.5
+fontColor = (255, 255, 255)
+lineType = 1
 for cnt in contours:
     M = cv2.moments(cnt)
+
     cx = int(M['m10'] / M['m00'])
     cy = int(M['m01'] / M['m00'])
     cv2.putText(binary, str(i),
-                (cx-8,cy+5),
+                (cx - 8, cy + 5),
                 font,
                 fontScale,
                 fontColor,
                 lineType)
-    print("região: %2d   perímetro: %7.2f   área: %6d" % (i, cv2.arcLength(cnt,True), cv2.contourArea(cnt)))
-    i = i+1
+
+    area = cv2.contourArea(cnt)
+    if area < 1500:
+        smallObj.append(area)
+    elif area < 3000:
+        midObj.append(area)
+    else:
+        bigObj.append(area)
+
+    print("região: %2d   perímetro: %7.2f   área: %6d" % (i, cv2.arcLength(cnt, True), area))
+
+    i = i + 1
 
 cv2.imshow('Centroids', image)
 cv2.imwrite(resultsDirName + '/' + fileName[0] + '_' + 'centroids' + fileName[1], image)
-cv2.waitKey(20000) & 0xFF
+print("numero de regiões pequenas %2d" % len(smallObj))
+print("numero de regiões médias   %2d" % len(midObj))
+print("numero de regiões grandes  %2d" % len(bigObj))
 
+allAreas = smallObj + midObj + bigObj
+
+plt.hist(allAreas, bins=[0,1500,3000,max(allAreas)])  # arguments are passed to np.histogram
+plt.title("Histogram with 'auto' bins")
+plt.show()
+
+
+cv2.waitKey(20000) & 0xFF
