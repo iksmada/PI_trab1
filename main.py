88a402030f81ebc73ff99c18522af46ce330858d
Raphael Adamski <raphaeladamski@hotmail.com>
2018-03-25 18:48:17 -0300

    mark centroids

--- main.py
+++ main.py
@@ -34,12 +34,26 @@ cv2.waitKey(1000)
 
 #numbered image and stats
 image, contours, hierarchy = cv2.findContours(binary, 1, 2)
+del contours[-1]
 i=0
+font                   = cv2.FONT_HERSHEY_SIMPLEX
+fontScale              = 0.5
+fontColor              = (255,255,255)
+lineType               = 1
 for cnt in contours:
     M = cv2.moments(cnt)
+    cx = int(M['m10'] / M['m00'])
+    cy = int(M['m01'] / M['m00'])
+    cv2.putText(binary, str(i),
+                (cx-8,cy+5),
+                font,
+                fontScale,
+                fontColor,
+                lineType)
     print("região: %2d   perímetro: %7.2f   área: %6d" % (i, cv2.arcLength(cnt,True), cv2.contourArea(cnt)))
     i = i+1
 
 cv2.imshow('Centroids', image)
 cv2.imwrite(resultsDirName + '/' + fileName[0] + '_' + 'centroids' + fileName[1], image)
-cv2.waitKey(1000)
+cv2.waitKey(20000) & 0xFF
+
